39e0acf4c1ad8761387920bca880ff463ba5ad57
bdcii <darrell.chess@gmail.com>
2021-03-01 17:26:35 -0500

    updated js after working with my tutor over the last couple weeks. I was also able to simplify the code dramatically!

--- script.js
+++ script.js
@@ -1,118 +1,73 @@
 
-
+let lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let number = '0123456789'
+let symbol = '!@#$%^&*()'
+let combinedArray = ''
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
-//  function writePassword() {
-var password = generatePassword();
+  //    function writePassword() {
+  //    var password = generatePassword();
 var passwordText = document.querySelector("#password");
+// symbol.split('').forEach(element => {
+//   console.log(element)
+//});
 
-//    passwordText.value = password;
-//  }
-
-
-
-// const passwordText = document.querySelector("#password");
-
-  const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", () =>
   {
-        //the following code prompts the user to seledt Password paramaters upon clicking the Generate button
+        //the following code prompts the user to select Password parameters upon clicking the Generate button
       let pwLength = prompt("How long is your password? Must be between 8 and 128 characters.");
-          if (pwLength === null || pwLength === undefined) return;
+          if (!pwLength) return;
           if (pwLength < 8 || pwLength > 128) {
           alert ("This does not meet the requirements! Try again.");
           return;
           } else {console.log(pwLength);  
           }
+        
+    
+
 
       let pwLower = window.confirm("Will your password include lowercase letters? *Required");
           if (pwLower === false) {
           alert ("Lowercase is required! Start over."); return;
-          } else {console.log(pwLower);
+          } else {combinedArray += lowerCase;
           }
 
       let pwUpper = window.confirm("Will your password include uppercase letters? *Required");
           if (pwUpper === false) {
           alert ("Uppercase is required! Start over."); return;
-          } else {console.log(pwUpper);
+          } else {combinedArray += upperCase;
           }
 
       let pwNumber = window.confirm("Will your password include numbers? *Required"); 
-          if (pwNumber === false) {
+          if (!pwNumber) {
           alert ("Numbers are required! Start over."); return;
-          } else {console.log(pwNumber);
+          } else {combinedArray += number;
           }
 
       let pwSymbol = window.confirm("Will your password include symbols? *Optional");
-          console.log(pwSymbol);
+          if (pwSymbol){
+            combinedArray += symbol;
+          }
 
-      
-      const length = +pwLength.value;
-     
-      const hasLower = (pwLower === true);
-      const hasUpper = (pwUpper === true);
-      const hasNumber = (pwNumber === true);
-      const hasSymbol = (pwSymbol == true);
-
-      passwordText.value = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length); //***
-
+        
+         
+            let generatedPassword = '';
+            
+            
+            for (let i = 0; i < parseInt(pwLength); i++){
+              let randomNumber = Math.floor(Math.random() * combinedArray.length)
+             generatedPassword += combinedArray[randomNumber]
+            }
+            passwordText.value = generatedPassword 
+    
+  
   });
 
-    function generatePassword(lower, upper, number, symbol, length){
 
-      let generatedPassword = '';
-
-      const typesCount = lower + upper + number + symbol;
-
-      console.log('typesCount: ', typesCount);
-
-      const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
-      (
-          item => Object.values(item)[0]
-      );
-
-      console.log('typesArr: ', typesArr);
-
-      for(let i = 0; i < length; i += typesCount){
-        typesArr.forEach(type => {
-          const funcName = Object.keys(type)[0];
-          console.log('funcName: ', funcName);
-
-          generatedPassword += randomFunc[funcName]();
-        });
-      }
-      const finalPassword = generatedPassword.slice(0, length);
-     return finalPassword;
-    }
-
-    //Generator functions to get random characters
-  function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}   
-    console.log(getRandomLower());
-
-function getRandomUpper() {
-        return String.fromCharCode(Math.floor(Math.random() * 26) + 65); //Uppercase start at spot 65 in the browser character set.
-}
-    console.log(getRandomUpper());
-    
-function getRandomNumber() {
-        return String.fromCharCode(Math.floor(Math.random() * 10) + 48); //48 is where numbers start in browser character set
-}
-    console.log(getRandomNumber());
-
-function getRandomSymbol() {
-        const symbols = '!@#$String.f%^&*(){}[]=<>/,.';
-        return symbols[Math.floor(Math.random() * symbols.length)];
-}
-    console.log(getRandomSymbol());    
